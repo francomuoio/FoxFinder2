6a0e75948b72528fca05f876b23e7bdd26386160
Michalski Lucas <lucas.michalski@epitech.eu>
2018-06-25 17:39:10 +0200

    basic implémentation devise user & company

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :companies, path: 'companies'
+  devise_for :users, path: 'users', controllers:
+  {
+    sessions: "users/sessions",
+    passwords: "users/passwords",
+    registrations: "users/registrations"
+  }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
