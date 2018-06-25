93b6643fc2cfbd7b8a7e7dd88a861415a2784cce
francomuoio <franco.59171@gmail.com>
2018-06-25 12:22:29 +0200

    first commit
    first commit

---
+++ db/migrate/20180625095326_add_info_to_user.rb
@@ -0,0 +1,8 @@
+class AddInfoToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :phone, :string
+    add_column :users, :address, :string
+    add_column :users, :property_type, :string
+    add_column :users, :room_nbr, :integer
+  end
+end
